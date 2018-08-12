35081cad26873e35846d144708e6eb49871e7112
real_marcool <marcul@poczta.onet.pl>
2018-08-12 11:49:57 +0200

    init

---
+++ src/test/java/com/realmarcool/sda/AppTest.java
@@ -0,0 +1,18 @@
+package com.realmarcool.sda;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
